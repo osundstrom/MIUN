4a205e78bbf2899e66da15c3bd5312410745fe4b
Oskar Sundström <ossu2300@student.miun.se>
2024-01-18 21:25:15 +0100

    updated version of sharp and resintall npm.

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -579,6 +579,23 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"gLLPy":[function(require,module,exports) {
+"use strict";
+//divForClock kopplas till element med id clockDiv
+let divForClock = document.getElementById("clockDiv");
+//Körs då sidan laddas in.
+window.onload = clock();
+//Funktion för klocka
+function clock() {
+    let newDate = new Date();
+    let hours = newDate.getHours();
+    let minutes = newDate.getMinutes();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    let time = `${hours}:${minutes}`;
+    console.log(time);
+    let textForClock = document.createTextNode(time);
+    divForClock.appendChild(textForClock);
+}
 
 },{}]},["75sNA","gLLPy"], "gLLPy", "parcelRequire2f92")
 
